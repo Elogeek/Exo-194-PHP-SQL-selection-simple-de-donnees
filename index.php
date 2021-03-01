8e84d340faa25acd874fdcb7bc018be00dde47f9
elogeek <elodiechristin@gmail.com>
2021-03-01 14:09:08 +0100

    exo les selections simples SQL

--- index.php
+++ index.php
@@ -13,3 +13,30 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+$server = 'localhost';
+$bdd = 'table_test_deux';
+$user = 'root';
+$password = 'dev';
+
+try {
+    $bdd = new PDO("mysql:host=$server;dbname=$bdd;charset=utf8",$user,$password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+    $stmt = $bdd->prepare("SELECT * from user");
+    $state = $stmt->execute();
+    if($state) {
+        foreach($stmt->fetchAll() as $user) {
+            echo"<div class='classe-css-utilisateur' style='background-color: aquamarine' style='border: inset chartreuse' style='font-size: x-large'> Utilisateur: " .$user['nom'] ." " .$user['prenom']." ".$user['rue']." ".$user['numero']." "
+                .$user['code_postal']." ".$user['ville']." ".$user['pays']." ".$user['mail'];
+        }
+    }
+    else {
+       echo "Une erreur est apparue!";
+    }
+
+}
+catch(PDOException $exception) {
+    echo $exception->getMessage();
+
+}
