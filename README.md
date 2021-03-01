79876f6ca4bc2ac66b4ed6fc85712e7b2dd833cc
elogeek <elodiechristin@gmail.com>
2021-03-01 16:31:07 +0100

    exo select données SQL

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Exo-194-PHP-SQL-selection-simple-de-donnees
+# Exo-194-PHP-SQL-se**/
+* 1. Importez le fichier SQL se trouvant dans le dossier SQL.
+* 2. Connectez vous à votre base de données avec PHP
+* 3. Sélectionnez tous les utilisateurs et affichez toutes les infos proprement dans un div avec du css
+*    ex:  <div class="classe-css-utilisateur">
+     *              utilisateur 1, données ( nom, prenom, etc ... )
+     *         </div>
+*         <div class="classe-css-utilisateur">
+    *              utilisateur 2, données ( nom, prenom, etc ... )
+    *         </div>
+* 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
+* 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
+     **/lection-simple-de-donnees
--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
-
-/**
- * 1. Importez le fichier SQL se trouvant dans le dossier SQL.
- * 2. Connectez vous à votre base de données avec PHP
- * 3. Sélectionnez tous les utilisateurs et affichez toutes les infos proprement dans un div avec du css
- *    ex:  <div class="classe-css-utilisateur">
- *              utilisateur 1, données ( nom, prenom, etc ... )
- *         </div>
- *         <div class="classe-css-utilisateur">
- *              utilisateur 2, données ( nom, prenom, etc ... )
- *         </div>
- * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
- * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
- */
 $server = 'localhost';
 $bdd = 'table_test_deux';
 $user = 'root';
 $password = 'dev';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
 
 try {
     $bdd = new PDO("mysql:host=$server;dbname=$bdd;charset=utf8",$user,$password);
@@ -26,17 +25,23 @@ try {
     $stmt = $bdd->prepare("SELECT * from user");
     $state = $stmt->execute();
     if($state) {
-        foreach($stmt->fetchAll() as $user) {
-            echo"<div class='classe-css-utilisateur' style='background-color: aquamarine' style='border: inset chartreuse' style='font-size: x-large'> Utilisateur: " .$user['nom'] ." " .$user['prenom']." ".$user['rue']." ".$user['numero']." "
+        foreach($stmt->fetchAll() as $ligne) {
+            echo"<div class='classe-css-utilisateur'>";
+            echo "Utilisateur:" ." ".$user['nom'] ." " .$user['prenom'].
+                " ".$user['rue']." ".$user['numero']." "
                 .$user['code_postal']." ".$user['ville']." ".$user['pays']." ".$user['mail'];
+            echo "</div>";
         }
     }
-    else {
-       echo "Une erreur est apparue!";
-    }
-
 }
 catch(PDOException $exception) {
     echo $exception->getMessage();
-
 }
+?>
+
+</body>
+</html>
+
+
+
+
